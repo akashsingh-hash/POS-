3ff9e9cdf2d975a43d1310cad0de19fe503eb630
houseofstark67 <houseofstark67@gmail.com>
2026-02-17 23:21:04 +0530

    chore: initial POS project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold text-green-600">
+      Tailwind v4 is working
+    </h1>
+  );
+};
+
+export default App;
